8190ae331742bed932ee1c02300404a534f670b0
drshaggy <connorhughes@fastmail.com>
2020-02-11 16:41:39 +0000

    problem 13

---
+++ problem13.py
@@ -0,0 +1,12 @@
+from helpers import Timer
+import numpy as np
+
+if __name__ == "__main__":
+    timer = Timer()
+    timer.start()
+    with open('data/problem13.txt') as file:
+        list_ = []
+        for line in file:
+            list_.append(int(line))
+    print(str(sum(list_))[0:10])
+    timer.stop()
